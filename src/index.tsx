da8e72de7536e259f65a698cade8d257a9477fb0
Bryan Villarubia <jemian868@gmail.com>
2024-03-20 14:24:55 +0800

    [Task][Fe] Create Input Component

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,18 @@
 import { StatusBar, Text, View } from "react-native";
+import CustomInput from "./components/Input";
+import { useForm } from "react-hook-form";
+import { Credentials } from "./state/reducers/hrmis/loginSlice";
 
 export default function App() {
+  const { control } = useForm<Credentials>({
+    defaultValues: {},
+  });
+
   return (
     <View>
       <Text> First View </Text>
+      <CustomInput control={control} name="Sample Input" />
+
       <StatusBar
         backgroundColor="maroon"
         barStyle="light-content"
